eb2dafa07476abdc1c3a62d854400bf98ff3f171
Patricio Gonzalez <patote.gonzalez@gmail.com>
2020-09-18 11:12:23 -0300

    class to db

---
+++ src/main/java/com/platzi/platzimarket/persistance/entity/Categoria.java
@@ -0,0 +1,51 @@
+package com.platzi.platzimarket.persistance.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @Column(name = "id_categoria")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "estado")
+    private Boolean active;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+
+}
---
+++ src/main/java/com/platzi/platzimarket/persistance/entity/ComprasProducto.java
@@ -0,0 +1,10 @@
+package com.platzi.platzimarket.persistance.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+
+}
