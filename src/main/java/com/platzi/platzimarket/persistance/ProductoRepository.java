bfa3e786838affe00493856477e06a85b15c1556
Patricio Gonzalez <patote.gonzalez@gmail.com>
2020-09-18 21:35:07 -0300

    domain ready

---
+++ src/main/java/com/platzi/platzimarket/persistance/ProductoRepository.java
@@ -0,0 +1,56 @@
+package com.platzi.platzimarket.persistance;
+
+import com.platzi.platzimarket.domain.Product;
+import com.platzi.platzimarket.domain.repository.ProductRepository;
+import com.platzi.platzimarket.persistance.crud.ProductoCrudRepository;
+import com.platzi.platzimarket.persistance.entity.Producto;
+import com.platzi.platzimarket.persistance.mapper.ProductMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository implements ProductRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll() {
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos =  productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+
+
+
+
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+}
